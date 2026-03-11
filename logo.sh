c02d9b378c19ca75225b922d52ed674ee198fc22
zero5yt <roderickalmaras05@gmail.com>
2026-03-12 01:34:30 +0800

    Fix echo statements and remove unnecessary clears

--- logo.sh
+++ logo.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-clear
 # Simple color animation effect
 for i in {1..3}; do
-    echo -e "\e[1;32m   WELCOME TO ALMARAS-TOOLS V1.0   \e[0m"
+    echo -e "\e[1;32m   WELCOME TO ALMARAS-TOOLS V1.0 \e[0m"
     sleep 0.2
-    clear
-    echo -e "\e[1;36m   WELCOME TO ALMARAS-TOOLS V1.0   \e[0m"
+    # DITO DAPAT WALA ANG 'clear'
+    echo -e "\e[1;36m   WELCOME TO ALMARAS-TOOLS V1.0 \e[0m"
     sleep 0.2
-    clear
 done
-echo -e "\e[1;32m   WELCOME TO ALMARAS-TOOLS V1.0   \e[0m"
+echo -e "\e[1;32m   WELCOME TO ALMARAS-TOOLS V1.0 \e[0m"
