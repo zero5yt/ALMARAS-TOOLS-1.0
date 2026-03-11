a516b09c6a8416518a6148f1c20d391f6f3f725c
zero5yt <roderickalmaras05@gmail.com>
2026-03-12 01:40:28 +0800

    Refactor logo.sh for cleaner output
    Removed unnecessary comments and simplified logo display.

--- logo.sh
+++ logo.sh
@@ -1,11 +1,9 @@
-# logo.sh - Walang 'clear' dito
-# I-print ang logo nang may animation
+# logo.sh
 for i in {1..3}; do
-    # \e[H = Ibalik ang cursor sa home (pinakataas na kaliwa)
-    echo -e "\e[H\e[1;32m   WELCOME TO ALMARAS-TOOLS V1.0 \e[0m"
+    echo -e "\e[1;32m   WELCOME TO ALMARAS-TOOLS V1.0 \e[0m"
     sleep 0.2
-    echo -e "\e[H\e[1;36m   WELCOME TO ALMARAS-TOOLS V1.0 \e[0m"
+    echo -e "\e[1;36m   WELCOME TO ALMARAS-TOOLS V1.0 \e[0m"
     sleep 0.2
 done
-# Siguraduhin na naka-set sa default color pagkatapos ng animation
-echo -e "\e[H\e[1;32m   WELCOME TO ALMARAS-TOOLS V1.0 \e[0m"
+# Siguraduhin na natatapos ang logo sa isang static na kulay
+echo -e "\e[1;32m   WELCOME TO ALMARAS-TOOLS V1.0 \e[0m"
