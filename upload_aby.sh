8df2b54ce19c008ed7a7af8c566ff7ab9e7b340f
zero5yt <roderickalmaras05@gmail.com>
2026-04-12 11:03:36 +0800

    Update upload_aby.sh

--- upload_aby.sh
+++ upload_aby.sh
@@ -4,9 +4,14 @@
 KEY_FILE="abyss_key.txt"
 FLD_ID="vwJ2zBCl5c"
 
-# 1. Hingi ng API Key kung wala pa
+# Siguraduhin na may pv
+if ! command -v pv &> /dev/null; then
+    echo "Installing pv for progress bar..."
+    pkg install pv -y
+fi
+
+# 1. Hingi ng API Key
 if [ ! -f "$KEY_FILE" ]; then
-    echo "--- ABYSS SETUP ---"
     read -p "Enter Aby API Key: " abyss_key
     echo "$abyss_key" > "$KEY_FILE"
 fi
@@ -14,32 +19,37 @@ fi
 ABYSS_API_KEY=$(cat "$KEY_FILE")
 TARGET=$1
 
-# 3. Check kung local file ba o URL ang pinasa
 if [ -f "$TARGET" ]; then
-    # Kung File sa folder
     FILENAME=$(basename "$TARGET")
     FILE_PATH="$TARGET"
-    echo "Using local file: $FILENAME"
 else
-    # Kung URL (Download muna)
     FILENAME=$(basename "$TARGET" | sed 's/%20/ /g')
     FILE_PATH="$FILENAME"
     echo "Downloading $FILENAME..."
-    wget -O "$FILENAME" "$TARGET"
+    wget -q --show-progress -O "$FILENAME" "$TARGET"
 fi
 
-# 4. Upload sa Aby
 if [ -f "$FILE_PATH" ]; then
-    echo "Uploading to Aby..."
-#curl to loading
+    echo "Uploading to Aby... (Please wait for server response after 100%)"
+    
+    # Kukuha ng size
+    FILE_SIZE=$(stat -c%s "$FILE_PATH")
+    
+ echo -ne "Uploading Video: $FILENAME... "
 
-curl -# -F "file=@$FILE_PATH;filename=$FILENAME;type=video/mp4" -F "fld_id=$FLD_ID" "http://up.abyss.to/$ABYSS_API_KEY"
+curl -# -F "fld_id=$FLD_ID" \
+-F "file=@$FILE_PATH;filename=$FILENAME;type=video/mp4" \
+"http://up.abyss.to/$ABYSS_API_KEY" 2>&1 | tr '#' '='
+
+
+echo -e "\n[✔] Data sent! Waiting for Abyss to provide the link..."     
+
+    echo "Server Response: $RESPONSE"
  
-    # 5. Clean up (Burahin lang kung hindi ito galing sa ATOOLS folder)
     if [[ "$FILE_PATH" != /storage/emulated/0/Download/ATOOLS/* ]]; then
         rm "$FILE_PATH"
     fi
     echo -e "\nUpload to Aby Done!"
 else
-    echo "Error: File/Download failed."
+    echo "Error: File not found."
 fi
